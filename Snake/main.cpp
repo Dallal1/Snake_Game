870d95769765522313ad8d726f1168779ae5dacc
Dallal1 <abw090571@gmail.com>
2023-12-19 22:22:25 +0100

    Creat The Foode

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,22 +1,67 @@
 #include "raylib.h"
 #include <iostream>
 
+using namespace std;
+
+//Color
+Color green = { 173, 204, 96, 255 };
+Color darkGreen = { 43, 51, 24, 255 };
+
+//Cell
+int cellSize = 30;
+int cellCount = 25;
+
+//Food
+class Foode
+{
+public:
+	Vector2 position;
+	Texture2D texture;
+
+	Foode()
+	{
+		Image image = LoadImage("Graphics/apll.png");
+		texture = LoadTextureFromImage(image);
+		UnloadImage(image);
+		position = GenerateRandomPos();
+	}
+
+	~Foode()
+	{
+		UnloadTexture(texture);
+	}
+
+	void Drawing()
+	{
+		DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+	}
+
+	Vector2 GenerateRandomPos()
+	{
+		float x = GetRandomValue(0, cellCount - 1);
+		float y = GetRandomValue(0, cellCount - 1);
+		return Vector2{ x, y };
+	}
+};
+
 int main()
 {
 	// Initialize window and OpenGL context
-	const int Screen_Widtth = 800;
-	const int Screen_Higth = 450;
-	InitWindow(Screen_Widtth, Screen_Higth, "Snake");
+	InitWindow(cellSize * cellSize, cellSize * cellCount, "Snake");
 
 	//FPS
 	SetTargetFPS(144);
+
+	Foode foode = Foode();
 
 	//Screen Loob
 	while (WindowShouldClose() == false)
 	{
 		BeginDrawing();
 
-			ClearBackground(BLACK);
+			//Drawing
+			ClearBackground(green);
+			foode.Drawing();
 		EndDrawing();
 	}
 
